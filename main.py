2bf004e002b2ffe7a717e6bacbcd7eb1e10bcbd0
bentleyjetlearn <bentleyron313@gmail.com>
2024-10-16 18:19:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#dictionary
+my_info = {
+   "name": "Bentley Ron, Pisani",
+    "age":"11",
+    "city":"Gharghur"
+}
+aiden_info = {
+
+    "name": "Aiden Bezzina",
+    "age": "10",
+    "city": "Birkirkara"
+}
+mikayel_info= {
+    "name":"Mikayel Attard",
+    "age":"13",
+    "city":"Zarbun"
+}
+nathan_info = {
+    "name":"Nathan Grech",
+    "age" :"5",
+    "city":"Naxxar"
+}
+print(my_info)
+print(aiden_info)
+print(mikayel_info)
+print(nathan_info)
